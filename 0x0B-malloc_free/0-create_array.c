01106eaf974199b4325bf069419548d77a140a37
Tweek-byte <zaka4396@gmail.com>
2023-09-04 17:30:37 -0700

    first task try2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,6 @@
  * create_array - Creates an array of characters
  * @size: The size of the array
  * @c: The character to initialize the array elements
- * @i: needed shit
  *
  * Return: On success, a pointer to the allocated array.
  *         On failure or if size is 0, returns NULL.
@@ -14,15 +13,13 @@
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return(NULL);
+	char *a = malloc(size);
 
-	char* a = malloc(size * sizeof(char);
-	if (a == NULL)
-		return(NULL);
+	if (a == NULL || size == 0)
+		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
-		a[i] = c;
+	while (size--)
+		a[size] = c;
 
-	return(a);
+	return (a);
 }
