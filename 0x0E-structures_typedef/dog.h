ec0de69090826ae7d50a73dbe8cbb165a4bb1ed8
Tweek-byte <zaka4396@gmail.com>
2023-09-11 17:10:55 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,25 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef	DOG_H
+#define	DOG_H
 
+/**
+ *  struct dog - dog structure
+ *  @name: dog's name
+ *  @age: dog's age
+ *  @owner: dog's owner
+ */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
-}
+	char *owner;
+};
 
-#endif /*DOG_H*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
