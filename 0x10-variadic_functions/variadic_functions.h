97c1dc1d10a4fc571c9a38eff76d23c86ad73a47
Tweek-byte <zaka4396@gmail.com>
2023-09-14 13:58:07 -0700

    t

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,8 @@
-#ifndef VRIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-typedef struct string
-{
-	char *str;
-	void (*print)(va_list arg);
-} to_string;
+#ifndef variadic_functions_H
+#define variadic_functions_H
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-
 #endif
