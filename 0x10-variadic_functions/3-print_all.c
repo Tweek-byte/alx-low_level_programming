a876a9d6ca8c2da7ed238ebf7988da02d15bb86a
Tweek-byte <zaka4396@gmail.com>
2023-09-15 14:08:25 -0700

    m

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,55 +1,47 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
- * print_all - prints arguments based on the format
- * @format: format string containing types of arguments
- *
- * Description: This function takes a format string and variable arguments
- *              and prints the arguments based on the format specified.
- *              The format string can contain the following types:
- *              - 'c': for char arguments
- *              - 'i': for integer arguments
- *              - 'f': for float arguments
- *              - 's': for string arguments (if NULL, "(nil)" is printed)
- *              The arguments are printed in the order specified.
- *              The function prints a newline character at the end.
+ * print_all - Prints arguments based on the format.
+ * @format: Format string containing types of arguments.
  *
  * Return: void
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0;
+	int i = 0, j = 0;
 	char *str;
-	char *separator = "";
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		j = 1;
+		switch (format[i])
 		{
-			printf("%s%c", separator, va_arg(args, int));
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%s%i", separator, va_arg(args, int));
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%s%f", separator, va_arg(args, double));
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", separator, str);
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				j = 0;
+				break;
 		}
 
-		separator = ", ";
+		if (format[i + 1] && j)
+			printf(", ");
 		i++;
 	}
 
