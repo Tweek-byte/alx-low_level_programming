1507f5522e0a2e1659491f78dcfa0782cc95c550
Tweek-byte <zaka4396@gmail.com>
2023-09-25 16:47:59 -0700

    Solution for task 2

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees list
+ *
+ * @head: pointer to the first node.
+ *
+ * Return: void.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
