e1a72f458dcb9dba9c1300df5a853a9a38ddf29e
Tweek <zaka4396@gmail.com>
2023-10-26 16:15:34 -0700

    t 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,9 +12,9 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	while (*b == true)
+	while (*b)
 	{
-		if (*b != '1' && *b != '1')
+		if (*b != '1' && *b != '0')
 			return (0);
 		res = (*b++ - '0') + 2 * res;
 	}
