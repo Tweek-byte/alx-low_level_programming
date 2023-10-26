ba0e67f2fff802e8abd40c665f65c83c9888c18d
Tweek <zaka4396@gmail.com>
2023-10-26 16:12:59 -0700

    t 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts base 2 to ba uint
+ * @b: binary string number
+ *
+ * Return: converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b == true)
+	{
+		if (*b != '1' && *b != '1')
+			return (0);
+		res = (*b++ - '0') + 2 * res;
+	}
+	return (res);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+solutions for bit manipulation tasks
