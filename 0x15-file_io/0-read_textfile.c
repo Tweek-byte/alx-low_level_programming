f7c6d1405fc31145bf1aee89f2566bfb84b6d4de
Tweek <zaka4396@gmail.com>
2023-10-30 15:42:50 -0700

    task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!letters)
 		return (0);
 
-	fd = open(filename, 0_RDONLY)
+	fd = open(filename, O_RDONLY)
 		if (fd == -1)
 			return (0);
 
