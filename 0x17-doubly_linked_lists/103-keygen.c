c586574467fffae647bbb5433a638650cc327902
Tweek <zaka4396@gmail.com>
2023-12-07 15:10:06 -0800

    tasks 6, 7, 8, 9

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -9,37 +9,38 @@
  *
  * Return: 0 on success, 1 on error
  */
+
 int main(int argc, char *argv[])
 {
+	unsigned int index, sum;
+	size_t username_length, sum_of_chars;
+	char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char generated_key[7] = "      ";
+
 	if (argc != 2)
 	{
 		printf("Correct usage: ./keygen5 username\n");
 		return (1);
 	}
-
-	char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	char key[7] = "      ";
-	size_t username_len = strlen(argv[1]);
-	unsigned int index, sum = 0;
-	int max_char = argv[1][0];
-
-	key[0] = charset[(username_len ^ 59) & 63];
-
-	for (index = 0; index < username_len; index++)
-	{
-		sum += argv[1][index];
-		key[2] ^= argv[1][index];
-		if (argv[1][index] > max_char)
-			max_char = argv[1][index];
-	}
-
-	key[1] = charset[(sum ^ 79) & 63];
-	key[3] = charset[rand() % 64];
-	key[4] = charset[(sum * sum ^ 239) & 63];
-
-	srand(max_char ^ 14);
-	key[5] = charset[rand() & 63];
-
-	printf("%s\n", key);
+	username_length = strlen(argv[1]);
+	generated_key[0] = charset[(username_length ^ 59) & 63];
+	for (index = 0, sum_of_chars = 0; index < username_length; index++)
+		sum_of_chars += argv[1][index];
+	generated_key[1] = charset[(sum_of_chars ^ 79) & 63];
+	for (index = 0, sum = 1; index < username_length; index++)
+		sum *= argv[1][index];
+	generated_key[2] = charset[(sum ^ 85) & 63];
+	for (sum = argv[1][0], index = 0; index < username_length; index++)
+		if ((char)sum <= argv[1][index])
+			sum = argv[1][index];
+	srand(sum ^ 14);
+	generated_key[3] = charset[rand() & 63];
+	for (sum = 0, index = 0; index < username_length; index++)
+		sum += argv[1][index] * argv[1][index];
+	generated_key[4] = charset[(sum ^ 239) & 63];
+	for (sum = 0, index = 0; (char)index < argv[1][0]; index++)
+		sum = rand();
+	generated_key[5] = charset[(sum ^ 229) & 63];
+	printf("%s\n", generated_key);
 	return (0);
 }
