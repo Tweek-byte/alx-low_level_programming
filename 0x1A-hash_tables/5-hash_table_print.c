45e144362fea577e13bb8c917e61baca28c42bdd
Tweek <zaka4396@gmail.com>
2023-12-22 15:27:43 -0800

    task 6

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *ptr;
+	unsigned long int i;
+	char flag = 0;
+
+	if (!ht->array || !ht)
+		return;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		ptr = ht->array[i];
+		while (ptr)
+		{
+			if (flag == 1)
+				printf(", ");
+
+			printf("'%s': '%s'", ptr->key, ptr->value);
+			flag = 1;
+			ptr = ptr->next;
+		}
+	}
+
+	printf("}\n");
+}
